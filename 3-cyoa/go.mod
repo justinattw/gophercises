67f1ac708b68acd6cfd88af67e22a4339bc3b335
Justin Wong <justinattw@icloud.com>
2024-08-16 13:32:48 +0100

    Set up cyoa starter code

---
+++ 3-cyoa/go.mod
@@ -0,0 +1,8 @@
+module cyoa.com
+
+go 1.21.5
+
+require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
+	github.com/gophercises/cyoa v0.0.0-20190722125433-070143bf424e
+)
---
+++ 3-cyoa/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+
+	// 1. Use the html/template package to create your HTML pages. Part of the purpose of this exercise is to get practice using this package.
+	// 2. Create an http.Handler to handle the web requests instead of a handler function.
+	// 3. Use the encoding/json package to decode the JSON file. You are welcome to try out third party packages afterwards, but I recommend starting here.
+	panic("")
+}
